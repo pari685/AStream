6cfc2ae9943d1816982453741622266bb00241b0
John Anderson <pari685@gmail.com>
2014-03-12 12:35:50 -0500

    added module to download segment

--- dist/client/dash_client.py
+++ dist/client/dash_client.py
@@ -3,13 +3,16 @@ Author:            Parikshit Juluri
 Contact:           pjuluri@mail.umkc.edu 
 
 
+Testing:
+    import dash_client
+    mpd_file = <MPD_FILE>
+    dash_client.playback_duration(mpd_file, 'http://198.248.242.16:8005/')
 """
-
 import read_mpd
+import urlparse
 import urllib2
 import thread
 from collections import defaultdict
-
 from multiprocessing import Lock, Process, Queue, current_process
 
 def get_mpd(url):
@@ -24,21 +27,39 @@ def get_mpd(url):
     mpd_file_handle.close()
     return mpd_file
 
+def get_domain_name(url):
+    """ Module to obtain the domain name from the URL
+        From : http://stackoverflow.com/questions/9626535/get-domain-name-from-url
+    """
+    parsed_uri = urlparse.urlparse(url)
+    domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    return domain
 
-
-
-def get_media(media, done_queue):
+def get_media(domain, media, done_queue):
     """ Download the media from the list of URL's in media
     http://toastdriven.com/blog/2008/nov/11/brief-introduction-multiprocessing/
     """
-    print "media.url_list"
-    print media.url_list
     for segment in [media.initialization] + media.url_list:
-        print "Downloading segment %s" % (segment)
+        segment_url = urlparse.urljoin(domain, segment)
+        print "Downloading segment %s" % (segment_url)
     return None
 
+def download_segment(segment_url):
+    """ Module to download the segement"""
+    try:
+        segment_data = urllib2.urlopen(segment_url).read()
+    except urllib2.HTTPError, e:
+        print "Unable to download DASH Segment file HTTP Error. Code", e.code
+        return None
+    parsed_uri = urlparse.urlparse(segment_url)
+    segment_path = '{uri.path}'.format(uri=parsed_uri)
+    segment_filename = segment_path.split('/')[-1]
+    segment_file_handle = open(segment_filename, 'wb')
+    segment_file_handle.write(segment_data)
+    segment_file_handle.close()
+    return segment_filename
 
-def start_playback(mpd_file):
+def start_playback(mpd_file, domain):
     """ Module to download the MPEG-DASH media"""
     dash_playback_object = read_mpd.DashPlayback()
     dash_playback_object = read_mpd.read_mpd(mpd_file, dash_playback_object)
@@ -50,60 +71,36 @@ def start_playback(mpd_file):
     # Download audio and video (All representations)
     audio_done_queues = defaultdict(Queue)
     video_done_queues = defaultdict(Queue)
-    
     processes = []
 
     for bandwidth in dash_audio:
-        dash_audio[bandwidth]= read_mpd.get_url_list(bandwidth, dash_audio[bandwidth], playback_duration)
-        p = Process(target=get_media, args=(dash_audio[bandwidth], audio_done_queues[bandwidth]))
-        p.start()
-        processes.append(p)
+        dash_audio[bandwidth] = read_mpd.get_url_list(
+                bandwidth, dash_audio[bandwidth],
+                playback_duration)
+        process = Process(target=get_media,
+                args=(domain, dash_audio[bandwidth],
+                    audio_done_queues[bandwidth]))
+        process.start()
+        processes.append(process)
 
     for bandwidth in dash_video:
-        dash_video[bandwidth]= read_mpd.get_url_list(bandwidth, dash_video[bandwidth], playback_duration)
-        p = Process(target=get_media, args=(dash_video[bandwidth], video_done_queues[bandwidth]))
-        p.start()
-        processes.append(p)
+        dash_video[bandwidth] = read_mpd.get_url_list(
+                bandwidth, dash_video[bandwidth],
+                playback_duration)
+        process = Process(target=get_media, args=(domain,
+                dash_video[bandwidth],
+                video_done_queues[bandwidth]))
+        process.start()
+        processes.append(process)
 
-    for p in processes:
-        p.join()
+    for process in processes:
+        process.join()
 
-    
-
-
-
-
-    
-    #for media in dash_playback_object.audio + dash_playback_object.video:
-
-    #for bandwidth in dash_audio:
-    #    read_mpd.get_url_list(bandwidth, dash_audio[bandwidth], dash_playback_object.playback_duration)
-    #    thread.start_new_thread(get_media, (dash_audio[bandwidth],))
-    #for bandwidth in dash_video:
-    #    read_mpd.get_url_list(bandwidth, dash_video[bandwidth], dash_playback_object.playback_duration)
-    #    thread.start_new_thread(get_media, (dash_video[bandwidth],))
+def main():
+    """ Main Program wrapper"""
+    url = 'http://198.248.242.16:8005/'
+    mpd_file = get_mpd(url)
+    domian = get_domain_name(url)
+    start_playback(mpd_file, domian)
 
 
-
-
-#def client(threadName, delay):
-#    """ Client """
-#    count = 0
-#    while count < 3:
-#        time.sleep(delay)
-#        count += 1
-#        print "%s %s" % (threadName, delay)
-#
-#
-#def start_thread():
-#    """ Sample"""
-#    try:
-#        thread.start_new_thread (client, ("Thread-1", 2,) )
-#        thread.start_new_thread (client, ("Thread-2", 4,) )
-#    except:
-#        print "Error: Unable to start thread"
-#
-#    while True:
-#        pass
-#
-#
