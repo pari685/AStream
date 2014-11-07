dd7a76c5e46bf58c26ae0323ca516492d76621da
pari685 <pari685@gmail.com>
2014-11-06 19:57:13 -0600

    Added lock for the buffer
    Added Lock for the buffer and added logging information

--- dist/client/dash_buffer.py
+++ dist/client/dash_buffer.py
@@ -1,11 +1,12 @@
 import Queue
+import threading
 import time
 import dash_config
 
 # Duartions in seconds
 SEGMENT_DURATION = 4
 INITIAL_BUFFERING_DURATION = 5
-
+PLAYER_STATES = ['INITIAL_BUFFERING', 'PLAY', 'PAUSE', 'BUFFERING', 'STOP', 'END']
 # segment = {'playback_length' : 4,
 #            'size' : 1024,
 #            'bitrate': 120
@@ -18,76 +19,99 @@ class DashBuffer:
         TODO: Progressbar https://github.com/slok/pygressbar
     """
     def __init__(self, video_length):
-        #PRE_START, INITIAL_BUFFERING, PLAY, PAUSE, BUFFERING, STOP, END
+        self.player_thread = None
         self.playback_start_time = None
         self.current_playback_time = 0
         self.playback_duration = video_length
-        self.playback_state = "PRE-START"
-        self.user_action = None
+        self.playback_state = "INITIAL_BUFFERING"
+        # Unlimited Buffer size
         self.max_buffer_size = video_length
-        self.current_buffer = Queue.Queue()
+        # Duration of the current buffer
+        self.buffer_length = 0
+        self.buffer_length_lock = threading.RLock()
+        # Current buffer that holds the segment data
         self.buffer = Queue.Queue()
+        self.buffer_lock = threading.RLock()
         self.current_segment = None
 
-    def player_time(self):
+    def set_state(self, state):
+        """ Function to set the state of the player"""
+        state = state.upper()
+        if state in PLAYER_STATES:
+            self.playback_state = state
+            dash_config.LOG.info("State set to {}".format(state))
+        else:
+            dash_config.LOG.error("Unidentified state: {}".format(state))
+
+    def initialize_player(self):
         """ function that update the current playback time"""
-        start_player = time.time()
-        while not self.current_buffer.empty():
+        start_time = time.time()
+        initial_wait = 0
+        #while not self.current_buffer.empty():
+        while True:
             if self.playback_state == "STOP":
                 # If video is stopped quit updating the playback time and exit player
-                config_dash.LOG.INFO("Player Stopped")
+                config_dash.LOG.info("Player Stopped at time {}".format(
+                    time.time() - start_time))
                 return 0
             elif self.playback_state == "PAUSE":
-                # do not update the playkack time. Wait for the state to change
+                # do not update the playback time. Wait for the state to change
+                config_dash.LOG.info("Player Paused after {} seconds of playback".format(self.current_playback_time))
                 continue
             if self.playback_state == "INITIAL_BUFFERING":
-                if self.current_buffer.qsize() * SEGMENT_DURATION < INITIAL_BUFFERING_DURATION:
-                    initial_wait = time.time() - start_player
+                if self.buffer.qsize() * SEGMENT_DURATION < INITIAL_BUFFERING_DURATION:
+                    initial_wait = time.time() - start_time
                     continue
                 else:
+                    dash_config.LOG.info("Initial Waiting Time = {}".format(initial_wait))
+                    self.playback_state = "PLAY"
+            if self.playback_state == "PLAY":
                     now = time.time()
-                    future = now + self.current_buffer.get()
+                    # Acquire Lock on the buffer and read a segment for it
+                    self.buffer_lock.acquire()
+                    play_segment = self.current_buffer.get()
+                    self.buffer_lock.release()
+                    # time when the segment finishes playback
+                    future = now + play_segment['playback_length']
                     while time.time() < future:
+                        # If playback hasn't started yet, set the playback_start_time
                         if not self.playback_start_time:
                             self.playback_start_time = time.time()
-                            print "Started Playback"
+                            dash_config.LOG.info("Started playing with representation {}".format(
+                                segment['bitrate']))
+                        # Duration for which the video was played in seconds (integer)
                         int_time = int(time.time() - self.playback_start_time)
                         if self.current_playback_time < int_time:
                             self.current_playback_time = int_time
+                        else:
+                            dash_config.LOG.error("Playback time ahead")
                         if self.current_playback_time >= self.playback_duration:
-                            print "Completed Video Playback"
+                            dash_config.LOG.info("Completed the video playback: {} seconds".format(self.current))
                             return 1
-
-
-    def player_manager(self):
-        while True:
-            if playback_state == "INITIAL_BUFFERING":
-                if self.current_buffer >= INITIAL_BUFFER_DURATION:
-                    playback_state = "PLAY"
-            if playback_state in ["PLAY", "BUFFERING"]:
-                if not self.buffer.empty():
-                    self.current_segment = self.buffer.get()
-                    self.current_buffer.put(current_segment['playback_length'])
-
-
-
-
-
-
-
-
-
-
-
+                    else:
+                        self.buffer_length_lock.release()
+                        self.buffer_length -= play_segment['playback_length']
+                        self.buffer_length_lock.release()
 
     def write(self, segment):
-        """ write segment to the buffer"""
+        """ write segment to the buffer.
+            Segment is dict with keys ['data', 'bitrate', 'playback_length', 'URI', 'size']
+        """
+        # Acquire Lock on the buffer and add a segment to it
+        self.buffer_lock.acquire()
         self.buffer.put(segment)
-        self.current_buffer += int(segment[playback_length])
+        self.buffer_lock.release()
+        self.buffer_length_lock.acquire()
+        self.buffer_length += int(segment[playback_length])
+        self.buffer_length_lock.release()
 
     def start(self):
         """ Start playback"""
-        self.playback_state = "PLAY"
+        self.playback_state = "INITIAL_BUFFERING"
+        dash_config.LOG.info("Starting the playback")
+        self.player_thread = threading.Thread(target=initialize_player)
+
+
 
 
 
