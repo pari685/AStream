3e13984d372587354f5c33119795bfb793c3f211
pari685 <pari685@gmail.com>
2014-11-12 22:18:58 -0600

    Config file for the ping

--- dist/client/config_dash.py
+++ dist/client/config_dash.py
@@ -1,7 +1,19 @@
+
 # create logger
 LOG_NAME = 'dash_log'
 LOG_LEVEL = None
 # Set '-' to print to screen
 LOG_FILENAME = '-'
-# Set 
+# Set
 LOG = None
+
+
+# For ping.py
+PING_PACKETS = 10
+ping_option_nb_pkts = PING_PACKETS
+rtt_match = None
+rtt_pattern = None
+index_rtt_min = None
+index_rtt_avg = None
+index_rtt_max = None
+RTT = False
