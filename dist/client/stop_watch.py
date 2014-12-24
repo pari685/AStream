f8d0a7a64e678541a4b93b00539f561e69496397
Parikshit Juluri <pari685@gmail.com>
2014-12-24 14:27:24 -0600

    minor (stop_watch)

--- dist/client/stop_watch.py
+++ dist/client/stop_watch.py
@@ -6,31 +6,31 @@ class StopWatch():
         Modified from http://code.activestate.com/recipes/124894-stopwatch-in-tkinter/
     """
     def __init__(self):
-        self._start = 0.0
-        self._elapsed_time = 0.0
-        self._running = 0
+        self.start_time = 0.0
+        self.elapsed_time = 0.0
+        self.running = 0
 
     def start(self):
         """ Start the stopwatch, ignore if running. """
-        if not self._running:
-            self._start = time.time() - self._elapsed_time
-            self._running = 1
+        if not self.running:
+            self.start_time = time.time() - self.elapsed_time
+            self.running = 1
     
     def pause(self):
         """ Stop the stopwatch, ignore if already paused."""
-        if self._running:
-            self._elapsed_time = time.time() - self.start
-            self._running = 0
+        if self.running:
+            self.elapsed_time = time.time() - self.start_time
+            self.running = 0
     
     def reset(self):
         """ Reset the stopwatch. """
-        self._start = time.time()
-        self._elapsed_time = 0.0
+        self.start_time = time.time()
+        self.elapsed_time = 0.0
 
     def time(self):
         """
         :return: elapsed time
         """
-        if self._running:
-            self._elapsed_time = time.time() - self._start
-        return int(self._elapsed_time)
+        if self.running:
+            self.elapsed_time = time.time() - self.start_time
+        return int(self.elapsed_time)
