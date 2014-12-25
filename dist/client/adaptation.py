95ee44c29fce2b0bbfef1ddf915f1352777f4d97
Parikshit Juluri <pari685@gmail.com>
2014-12-25 00:56:11 -0600

    Change available_duration and available_segments

--- dist/client/adaptation.py
+++ dist/client/adaptation.py
@@ -139,30 +139,32 @@ def weighted_dash(bitrates, dash_player, weighted_dwn_rate, curr_bitrate, next_s
     # Waiting time before downloading the next segment
     delay = 0
     next_bitrate = None
-    available_video = dash_player.buffer.qsize() - dash_player.initial_buffer
+    available_video_segments = dash_player.buffer.qsize() - dash_player.initial_buffer
     # If the buffer is less that the Initial buffer, playback remains at th lowest bitrate
     # i.e dash_buffer.current_buffer < dash_buffer.initial_buffer
-    config_dash.LOG.debug("Buffer_length = {} Initial Buffer = {} Available video = {}, alpha = {}. "
+    available_video_duration = available_video_segments * dash_player.segment_duration
+    config_dash.LOG.debug("Buffer_length = {} Initial Buffer = {} Available video = {} seconds, alpha = {}. "
                           "Beta = {} WDR = {}, curr Rate = {}".format(dash_player.buffer.qsize(),
                                                                       dash_player.initial_buffer,
-                                                                      available_video, dash_player.alpha,
+                                                                      available_video_duration, dash_player.alpha,
                                                                       dash_player.beta, weighted_dwn_rate,
-                                                                      next_segment_sizes[curr_bitrate]))
-    if weighted_dwn_rate == 0 or available_video == 0:
+                                                                      curr_bitrate))
+
+    if weighted_dwn_rate == 0 or available_video_segments == 0:
         next_bitrate = bitrates[0]
     # If time to download the next segment with current bitrate is longer than current - initial,
     # switch to a lower suitable bitrate
 
-    elif float(next_segment_sizes[curr_bitrate])/weighted_dwn_rate > available_video:
+    elif float(next_segment_sizes[curr_bitrate])/weighted_dwn_rate > available_video_duration:
         config_dash.LOG.debug("next_segment_sizes[curr_bitrate]) weighted_dwn_rate > available_video")
         for bitrate in reversed(bitrates):
             if bitrate < curr_bitrate:
-                if float(next_segment_sizes[bitrate])/weighted_dwn_rate < available_video:
+                if float(next_segment_sizes[bitrate])/weighted_dwn_rate < available_video_duration:
                     next_bitrate = bitrate
                     break
         if not next_bitrate:
             next_bitrate = bitrates[0]
-    elif available_video <= dash_player.alpha:
+    elif available_video_segments <= dash_player.alpha:
         config_dash.LOG.debug("available_video <= dash_player.alpha")
         if curr_bitrate >= max(bitrates):
             config_dash.LOG.info("Current bitrate is MAX", curr_bitrate)
@@ -171,36 +173,36 @@ def weighted_dash(bitrates, dash_player, weighted_dwn_rate, curr_bitrate, next_s
             higher_bitrate = bitrates[bitrates.index(curr_bitrate)+1]
             # Jump only one if suitable else stick to the current bitrate
             config_dash.LOG.info("next_segment_sizes[higher_bitrate] = {}, weighted_dwn_rate = {} , "
-                                 "available_video={}, ratio = {}".format(next_segment_sizes[higher_bitrate],
-                                                                             weighted_dwn_rate,
-                                                                             available_video,
-                                                                             float(next_segment_sizes[higher_bitrate])/
-                                                                             weighted_dwn_rate))
-            if float(next_segment_sizes[higher_bitrate])/weighted_dwn_rate < available_video:
+                                 "available_video={} seconds, ratio = {}".format(next_segment_sizes[higher_bitrate],
+                                                                                 weighted_dwn_rate,
+                                                                                 available_video_duration,
+                                                                                 float(next_segment_sizes[
+                                                                                     higher_bitrate])/weighted_dwn_rate))
+            if float(next_segment_sizes[higher_bitrate])/weighted_dwn_rate < available_video_duration:
                 next_bitrate = higher_bitrate
             else:
                 next_bitrate = curr_bitrate
-    elif available_video <= dash_player.beta:
+    elif available_video_segments <= dash_player.beta:
         config_dash.LOG.debug("available_video <= dash_player.beta")
         if curr_bitrate >= max(bitrates):
             next_bitrate = curr_bitrate
         else:
             for bitrate in reversed(bitrates):
                 if bitrate >= curr_bitrate:
-                    if float(next_segment_sizes[bitrate])/weighted_dwn_rate < available_video:
+                    if float(next_segment_sizes[bitrate])/weighted_dwn_rate < available_video_duration:
                         next_bitrate = bitrate
                         break
             if not next_bitrate:
                 next_bitrate = curr_bitrate
 
-    elif available_video > dash_player.beta:
+    elif available_video_segments > dash_player.beta:
         config_dash.LOG.debug("available_video > dash_player.beta")
         if curr_bitrate >= max(bitrates):
             next_bitrate = curr_bitrate
         else:
             for bitrate in reversed(bitrates):
                 if bitrate >= curr_bitrate:
-                    if float(next_segment_sizes[bitrate])/weighted_dwn_rate > available_video:
+                    if float(next_segment_sizes[bitrate])/weighted_dwn_rate > available_video_duration:
                         next_bitrate = bitrate
                         break
         if not next_bitrate:
